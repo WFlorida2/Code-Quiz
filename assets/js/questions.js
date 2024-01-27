7494f762382dbe0b8210c12c1bec4e66dde4d3c9
Amr Shehata <amr_shehata@hotmail.com>
2024-01-27 02:03:27 +0000

    Added a pool of questions of 2 for now

---
+++ assets/js/questions.js
@@ -0,0 +1,13 @@
+        // Define an array to hold pool of questions
+        var quizQuestions = [
+            {
+            question: "What does the typeof operator in JavaScript return for this code: 'typeof 42'; ?",
+            choices: ["number", "string", "boolean", "undefined"],
+            correctAnswer: "number"
+            },
+            {
+            question: "Which of the following is the correct way to declare a function in JavaScript ?",
+            choices: ["function = myFunction() {}", "var myFunction = function() {}", "def myFunction() {}", "function myFunction() {}"],
+            correctAnswer: "function myFunction() {}"
+            },
+        ];
